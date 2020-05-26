8a827da0468d022fb4a5b395091bc7eb72ca2109
Thibaut Vandervelden <thvdveld@vub.be>
2020-05-26 08:34:13 +0200

    Add UART peripheral mapping.

--- svd/src/lib.rs
+++ svd/src/lib.rs
@@ -7,6 +7,8 @@
 #![allow(clippy::doc_markdown)]
 #![allow(clippy::missing_errors_doc)]
 
+pub mod uart;
+
 pub use anyhow::{bail, Result};
 
 use drone_svd::{Config, Device};
@@ -38,9 +40,14 @@ fn svd_config() -> Result<Config<'static>> {
 fn svd_deserialize() -> Result<Device> {
     drone_svd::rerun_if_env_changed();
     match env::var("CARGO_CFG_TISL_MCU")?.as_ref() {
-        "cc2538" => parse_svd("cc2538sf53.svd"),
+        "cc2538" => patch_cc2538(parse_svd("cc2538sf53.svd")?),
         _ => bail!("invalid `tisl_mcu` cfg flag"),
     }
+}
+
+fn patch_cc2538(mut dev: Device) -> Result<Device> {
+    uart::remove_reserved(&mut dev)?;
+    Ok(dev)
 }
 
 fn parse_svd(path: &str) -> Result<Device> {
