186b1cd9819cc28961a8263e6c0befa1ad132d42
Thibaut Vandervelden <thvdveld@vub.be>
2020-05-27 11:05:52 +0200

    Add TIMER peripheral mappings for CC2538.

--- src/periph/mod.rs
+++ src/periph/mod.rs
@@ -2,3 +2,6 @@
 
 #[doc(no_inline)]
 pub use drone_cortexm::map::periph::*;
+
+#[cfg(feature = "tim")]
+pub extern crate drone_tisl_map_periph_tim as tim;
---
+++ tests/periph_macros.rs
@@ -0,0 +1,19 @@
+use drone_core::token::Token;
+use drone_tisl_map::tisl_reg_tokens;
+
+tisl_reg_tokens! {
+    struct Regs;
+}
+
+#[test]
+#[allow(unused_variables)]
+fn perip_macros() {
+    let reg = unsafe { Regs::take() };
+    #[cfg(all(feature = "tim", any(tisl_mcu = "cc2538")))]
+    {
+        let gptim0 = drone_tisl_map::periph::tim::periph_gptimer0!(reg);
+        let gptim1 = drone_tisl_map::periph::tim::periph_gptimer1!(reg);
+        let gptim2 = drone_tisl_map::periph::tim::periph_gptimer2!(reg);
+        let gptim3 = drone_tisl_map::periph::tim::periph_gptimer3!(reg);
+    }
+}
